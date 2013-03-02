ba12bfbd069c2e3395bb96c89183196d14d31f6b
sam <sam@sam-SVE1511AENB.(none)>
2013-03-02 23:09:50 +0530

    site version 1.0

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def home
+  end
+
+  def galary
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Operation::Application.config.secret_token = 'ea19fa0816f2b24dee2e6bcef4e4c4d5280ddc5604dceb3ffeef465a73e1081a4c4fd1f9704295ab60a8bd961177dc31436509a75d4a9077b5029530a7ac3ba8'
